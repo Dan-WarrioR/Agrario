8af6b04764a05fada603d267d0c083b74414d45e
DanWarrioR <norkinDanya2006@gmail.com>
2025-01-13 22:56:03 +0100

    Returned Circle and Text Objects

---
+++ Source/Game/Units/CircleObject.cs
@@ -0,0 +1,47 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using Source.Engine.GameObjects;
+using Source.Engine.Tools;
+
+namespace Source.Game.Units
+{
+    public class CircleObject : GameObject
+    {
+        protected virtual Color FillColor => Color.White;
+
+        public FloatRect ObjectRect => Circle.GetGlobalBounds();
+
+        public override Vector2f Position => Circle.Position;
+
+        public float Radius => Circle.Radius;
+
+        protected CircleShape Circle { get; }
+
+        public CircleObject(float radius, Vector2f initialPosition) : base(initialPosition)
+        {
+            Circle = new CircleShape(radius)
+            {
+                Position = initialPosition,
+                Origin = new(radius, radius),
+                FillColor = FillColor,
+            };
+        }
+
+        public bool IsIntersects(CircleObject sphere)
+        {
+            float distanceToObject = Position.DistanceTo(sphere.Position);
+
+            return distanceToObject <= Radius + sphere.Radius;
+        }
+
+        public bool IsIntersects(FloatRect objectRect)
+        {
+            return ObjectRect.Intersects(objectRect);
+        }
+
+        public override void Draw(RenderTarget target, RenderStates states)
+        {
+            Circle.Draw(target, states);
+        }
+    }
+}
---
+++ Source/Game/Units/TextObject.cs
@@ -0,0 +1,43 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using Source.Engine.GameObjects;
+
+namespace Source.Game.Units
+{
+    public class TextObject : GameObject
+    {
+        private static readonly Color DefaultTextColor = Color.Black;
+        private static readonly Font DefaultFont = new(FontPath);
+
+        private const string FontPath = @"C:\Windows\Fonts\Arial.ttf";
+        private const uint CharacterSize = 24;
+
+        public override Vector2f Position => _text.Position;
+
+        private Text _text;
+
+        public TextObject(string text, Vector2f initialPosition) : base(initialPosition)
+        {
+            _text = new(text, DefaultFont, CharacterSize)
+            {
+                FillColor = DefaultTextColor,
+                Position = initialPosition,
+            };
+        }
+
+        public TextObject(string text, Vector2f initialPosition, Color textColor) : this(text, initialPosition)
+        {
+            _text.FillColor = textColor;
+        }
+
+        public void OnScoreChanged(float playerMass)
+        {
+            _text.DisplayedString = playerMass.ToString();
+        }
+
+        public override void Draw(RenderTarget target, RenderStates states)
+        {
+            _text.Draw(target, states);
+        }
+    }
+}
