ccddc48c1ad410c4f90e7358266fc43dda7d32d0
DanWarrioR <norkinDanya2006@gmail.com>
2025-01-13 22:58:04 +0100

    Fixed Unit factory

--- Source/Engine/Factory/UnitFactory.cs
+++ Source/Engine/Factory/UnitFactory.cs
@@ -1,65 +1,75 @@
-﻿using SFML.Graphics;
+﻿using Source.Game.Units;
+using SFML.Graphics;
 using SFML.System;
 using Source.Engine.GameObjects;
-using Source.Game.Units;
 
 namespace Source.Engine.Factory
 {
-	public static class UnitFactory
+    public class UnitFactory
 	{
 		private const float MinPlayerRadius = 20f;
 
 		private const float FoodRadius = 5f;
 
-		private static Random _random = new();
+		private GameLoop _gameLoop;
 
-		public static Player CreatePlayer(bool isAi, FloatRect bounds)
+		private FloatRect _bounds;
+
+		private Random _random = new();
+
+		public UnitFactory(GameLoop gameLoop, FloatRect bounds)
 		{
-			var player = new Player(isAi, bounds, MinPlayerRadius, GetRandomPosition(bounds));
+			_gameLoop = gameLoop;
+			_bounds = bounds;
+		}
+
+		public Player CreatePlayer(bool isAi)
+		{
+			var player = new Player(isAi, _bounds, MinPlayerRadius, GetRandomPosition());
 
 			RegisterObject(player);
 
 			return player;
 		}
 
-		public static Food CreateFood(FloatRect bounds)
+		public Food CreateFood()
 		{
-			var food = new Food(FoodRadius, GetRandomPosition(bounds));
+			var food = new Food(FoodRadius, GetRandomPosition());
 
 			RegisterObject(food);	
 
 			return food;
 		}
 
-		public static TextObject CreateText(string text, FloatRect bounds)
+		public TextObject CreateText(string text)
 		{
-			var textObject = new TextObject(text, new(bounds.Width - 200, bounds.Top + 20));
+			var textObject = new TextObject(text, new(_bounds.Width - 200, _bounds.Top + 20));
 
-			RegisterObject(textObject);
+			//RegisterObject(textObject);
 
 			return textObject;
 		}
 
-		private static Vector2f GetRandomPosition(FloatRect bounds)
+		private Vector2f GetRandomPosition()
 		{
-			float x = _random.Next((int)bounds.Left, (int)(bounds.Left + bounds.Width));
-			float y = _random.Next((int)bounds.Top, (int)(bounds.Top + bounds.Height));
+			float x = _random.Next((int)_bounds.Left, (int)(_bounds.Left + _bounds.Width));
+			float y = _random.Next((int)_bounds.Top, (int)(_bounds.Top + _bounds.Height));
 			
 			return new(x, y);
 		}
 
-		private static void RegisterObject(GameObject gameObject)
+		private void RegisterObject(GameObject gameObject)
 		{
-			GameLoop.Register(gameObject);
+			_gameLoop.Register(gameObject);
 
-			gameObject.OnDisposed += InregisterObject;
+			gameObject.OnDisposed += UnregisterObject;
 		}
 
-		private static void InregisterObject(GameObject gameObject)
+		private void UnregisterObject(GameObject gameObject)
 		{
-			gameObject.OnDisposed -= InregisterObject;
+			gameObject.OnDisposed -= UnregisterObject;
 
-			GameLoop.UnRegister(gameObject);
+			_gameLoop.UnRegister(gameObject);
 		}
 	}
 }
