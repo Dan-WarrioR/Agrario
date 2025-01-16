32a6a08d4e44b5ef1e8fcecc24a580ce37327c2d
DanWarrioR <norkinDanya2006@gmail.com>
2025-01-16 15:14:26 +0100

    Fixed vector2f methods naming

--- Source/Engine/Tools/Vector2fExtensions.cs
+++ Source/Engine/Tools/Vector2fExtensions.cs
@@ -6,10 +6,15 @@ namespace Source.Engine.Tools
 	{
 		public static float DistanceTo(this Vector2f source, Vector2f second)
 		{
+			return MathF.Sqrt(DistanceToSquared(source, second));
+		}
+
+		public static float DistanceToSquared(this Vector2f source, Vector2f second)
+		{
 			float deltaX = second.X - source.X;
 			float deltaY = second.Y - source.Y;
 
-			return MathF.Sqrt(deltaX * deltaX + deltaY * deltaY);
+			return deltaX * deltaX + deltaY * deltaY;
 		}
 
 		public static Vector2f Normalize(this Vector2f source)
