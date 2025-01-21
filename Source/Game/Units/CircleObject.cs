03a6dcef586c4917114cf3fb8a2ce7be0a1fc82e
DanWarrioR <norkinDanya2006@gmail.com>
2025-01-21 16:59:40 +0100

    Created ShapeObject

--- Source/Game/Units/CircleObject.cs
+++ Source/Game/Units/CircleObject.cs
@@ -1,52 +1,27 @@
 ﻿using SFML.Graphics;
 using SFML.System;
-using Source.Engine.GameObjects;
 using Source.Engine.Tools;
 
 namespace Source.Game.Units
 {
-    public class CircleObject : GameObject
+    public class CircleObject : ShapeObject
     {
-        protected virtual Color FillColor => Color.White;
-
-        public FloatRect ObjectRect => Circle.GetGlobalBounds();
-
-        public Vector2f Position => Circle.Position;
-
         public float Radius => Circle.Radius;
 
         protected CircleShape Circle { get; }
 
-        public CircleObject(float radius, Vector2f initialPosition)
+        public CircleObject(float radius, Vector2f initialPosition) : base(new CircleShape(radius), initialPosition)
         {
-            Circle = new CircleShape(radius)
-            {
-                Position = initialPosition,
-                Origin = new(radius, radius),
-                FillColor = FillColor,
-            };
-        }
+            Circle = (CircleShape)Shape;
 
-		public void SetPosition(Vector2f position)
-		{
-			Circle.Position = position;
+            Shape.Origin = new(radius, radius);
 		}
 
 		public bool IsIntersects(CircleObject sphere)
         {
             float distanceToObject = Position.DistanceTo(sphere.Position);
-
+            
             return distanceToObject <= Radius + sphere.Radius;
-        }
-
-        public bool IsIntersects(FloatRect objectRect)
-        {
-            return ObjectRect.Intersects(objectRect);
-        }
-
-        public override void Draw(RenderTarget target, RenderStates states)
-        {
-            Circle.Draw(target, states);
         }
     }
 }
