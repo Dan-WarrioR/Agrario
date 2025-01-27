f1ad0bb6a55e1c1096d00beaea47ea958372ab76
DanWarrioR <norkinDanya2006@gmail.com>
2025-01-27 11:47:36 +0100

    Separated bind and input manager

---
+++ Source/Engine/Input/KeyBind.cs
@@ -0,0 +1,44 @@
+﻿using SFML.Window;
+
+namespace Source.Engine.Input
+{
+	public class KeyBind
+	{
+		public event Action OnKeyPressed;
+		public event Action OnKeyHeld;
+		public event Action OnKeyReleased;
+
+		public Keyboard.Key Key;
+
+		private bool _wasPressed;
+		private bool _isPressed;
+
+		public KeyBind(Keyboard.Key key)
+		{
+			Key = key;
+		}
+
+		public void Update()
+		{
+			_wasPressed = _isPressed;
+			_isPressed = Keyboard.IsKeyPressed(Key);
+
+			if (_isPressed)
+			{
+				if (!_wasPressed)
+				{
+					OnKeyPressed?.Invoke();
+				}
+
+				OnKeyHeld?.Invoke();
+			}
+			else
+			{
+				if (_wasPressed)
+				{
+					OnKeyReleased?.Invoke();
+				}
+			}
+		}
+	}
+}
--- Source/Engine/Input/PlayerInput.cs
+++ Source/Engine/Input/PlayerInput.cs
@@ -1,48 +1,8 @@
 ﻿using SFML.Window;
 using Source.Engine.Tools;
 
-namespace Agrario.Source.Engine.Input
+namespace Source.Engine.Input
 {
-    public class KeyBind
-    {
-        public event Action OnKeyPressed;
-        public event Action OnKeyHeld;
-        public event Action OnKeyReleased;
-
-        public Keyboard.Key Key;
-
-        private bool _wasPressed;
-        private bool _isPressed;
-
-        public KeyBind(Keyboard.Key key)
-        {
-            Key = key;
-        }
-
-        public void Update()
-        {
-            _wasPressed = _isPressed;
-            _isPressed = Keyboard.IsKeyPressed(Key);
-
-            if (_isPressed)
-            {
-                if (!_wasPressed)
-                {
-                    OnKeyPressed?.Invoke();
-                }
-
-                OnKeyHeld?.Invoke();
-            }
-            else
-            {
-                if (_wasPressed)
-                {
-                    OnKeyReleased?.Invoke();
-                }
-            }
-        }
-    }
-
     public class PlayerInput
     {
         private readonly Window _window;
